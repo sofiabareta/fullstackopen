563b88fc7c4895d9c11cb3b0072f9c7dfa98e9cb
Sofia Bareta <sofibareta@gmail.com>
2023-04-25 22:33:53 -0300

    [1.2] course information

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,6 +6,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercise}
     </p>
@@ -30,9 +40,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content 
+          part1={part1} exercises1={exercises1} 
+          part2={part2} exercises2={exercises2} 
+          part3={part3} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
