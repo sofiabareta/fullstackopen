a475f3354df0281409e0784611028aebd86eae54
Sofia <sofibareta@gmail.com>
2023-05-22 16:45:57 -0300

    [PART 3] B

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const requestLogger = (request, response, next) => {
@@ -15,6 +16,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(cors())
 
 let notes = [
     {
@@ -76,7 +78,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
