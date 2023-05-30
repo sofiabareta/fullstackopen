f2896b3d51c0aa4046b7df815c6958dbbe1471f2
Sofia <sofibareta@gmail.com>
2023-05-30 13:59:55 -0300

    [4.4] Run totalLikes test

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  const likes = blogs.reduce((acc, blog) => acc + blog.likes, 0)
+
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
