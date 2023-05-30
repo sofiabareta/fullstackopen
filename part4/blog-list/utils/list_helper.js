8d4b38abff167214eedaa1042d127cc0bb8f384b
Sofia <sofibareta@gmail.com>
2023-05-30 20:19:52 -0300

    [4.6] Run mostBlogs test

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -18,8 +18,23 @@ const favoriteBlog = blogs => {
   return { title: moreLikes.title, author: moreLikes.author, likes: moreLikes.likes }
 }
 
+const mostBlogs = blogs => {
+  let authorsList = {}
+
+  blogs.map(blog => {
+    const authorKey = blog.author
+    return authorsList[authorKey] = authorsList[authorKey] === undefined ? 1 : authorsList[authorKey] + 1
+  })
+
+  return Object.keys(authorsList).reduce((prev, current) => {
+    return authorsList[prev] > authorsList[current] ? { 'author': prev, 'blogs': authorsList[prev] } : { 'author': current, 'blogs': authorsList[current] }
+  })
+
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
