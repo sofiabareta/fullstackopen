fe905452808d17d2eeff83f9f0c6acd38da30e08
Sofia <sofibareta@gmail.com>
2023-05-31 22:18:13 -0300

    [4.11] Tests for blog list - step 4

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
@@ -22,7 +23,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static('build'))
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
