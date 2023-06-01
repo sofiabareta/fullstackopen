5841787fcf39dca920a8ca6a3cb7911ef70f5d2d
Sofia <sofibareta@gmail.com>
2023-05-31 21:01:03 -0300

    [4.10] Tests for blog list - step 3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
 
 test('all blogs are returned', async () => {
   await api
@@ -18,6 +29,25 @@ test('all blogs have "id" property as unique verifier', async () => {
 
   const content = result.body.map(item => item.id)
   expect(content).toBeDefined()
+})
+
+test('a new blog is added', async () => {
+  const newBlog = {
+    title: 'Blog da pofinha',
+    author: 'Pofinha',
+    url: 'http://pofinha',
+    likes: 4
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const notesAtEnd = await helper.blogsInDb()
+  expect(notesAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
 })
 
 
