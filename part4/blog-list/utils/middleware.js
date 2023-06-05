59de3403ab2844fe5e99d1fc45aafff917956d48
Sofia <sofibareta@gmail.com>
2023-06-05 15:54:21 -0300

    [4.22] Blog list expansions - step 10

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,4 +1,14 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const getAuth = (req) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -9,10 +19,21 @@ const requestLogger = (request, response, next) => {
 }
 
 const tokenExtractor = (request, response, next) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    request.token = authorization.replace('Bearer ', '')
+  const token = getAuth(request)
+  request.token = token
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const token = getAuth(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
+
+  request.user = await User.findById(decodedToken.id)
+
   next()
 }
 
@@ -38,5 +59,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
