8667fd5a324afd88fa9122ad82df4679776157d0
Sofia <sofibareta@gmail.com>
2023-06-07 22:01:35 -0300

    [5.9] Blog list frontend - step 9

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import blogService from '../services/blogs'
 
 const Blog = ({ blog, user }) => {
   const [show, setShow] = useState(false)
+  const [likes, setLikes] = useState(blog.likes || 0)
   
   const blogStyle = {
     paddingTop: 10,
@@ -11,14 +13,27 @@ const Blog = ({ blog, user }) => {
     marginBottom: 5
   }
 
+  const addLike = async () => {
+    blog.likes = 1 || blog.likes + 1
+
+    const response = await blogService.update(blog.id, blog)
+
+    try {
+      setLikes(response.likes)
+    } catch (exception){
+      console.log(exception)
+    }
+
+  }
+
   return (
     <div style={blogStyle}>
       <span>{blog.title} {blog.author}</span>
       <button onClick={() => setShow(!show)}>{show ? 'hide' : 'show'}</button>
       {show && <>
         <p>{blog.url}</p>
-        <span>likes {blog.likes}</span>
-        <button>like</button>
+        <span>likes {likes}</span>
+        <button onClick={addLike}>like</button>
         <p>{user}</p>
         </>
       }
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -30,5 +30,14 @@ const create = async newObj => {
   return response.data
 }
 
+const update = async (id, newObj) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.put(`${baseUrl}/${id}`, newObj, config)
+  return response.data
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, setToken, create }
+export default { getAll, setToken, create, update }
