fd13f802b747b3ca7141cf675bc8ad715a5e2ec0
Sofia <sofibareta@gmail.com>
2023-06-16 18:23:24 -0300

    [5.14] Blog list frontend - step 14

--- part5/bloglist-frontend/src/components/blog.test.js
+++ part5/bloglist-frontend/src/components/blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -26,10 +27,38 @@ test('renders content', () => {
 
   render(<Blog blog={blog} user={user} />)
 
-  screen.debug()
-
   const element = screen.getByText('Sofia')
   expect(element).toBeVisible()
 
   expect(screen.getByText('url test')).not.toBeVisible()
+})
+
+test('clicking the button shows URL and likes', async () => {
+  const blog = {
+    author: 'Sofia',
+    url: 'url test',
+    title: 'Title test',
+    likes: 2,
+    user: {
+      id: 111,
+      username: 'Username',
+      name: 'Name'
+    }
+  }
+
+  const blogUser = {
+    user: {
+      id: 111,
+      username: 'Username',
+      name: 'Name'
+    }
+  }
+
+  render(<Blog blog={blog} user={blogUser} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('show')
+  await user.click(button)
+  // screen.debug()
+  expect(screen.getByText('url test')).toBeVisible()
 })
