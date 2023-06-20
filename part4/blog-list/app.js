c68fb6a1595d0fd6f0857f1785c0ab0e4a7a4a14
Sofia <sofibareta@gmail.com>
2023-06-20 00:31:33 -0300

    [5.17] Blog list frontend - step 17 - backend

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -10,6 +10,11 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
