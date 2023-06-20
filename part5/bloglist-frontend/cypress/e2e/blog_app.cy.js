ac609c4698071cc93809c7e2ea71a955627a29ca
Sofia <sofibareta@gmail.com>
2023-06-20 00:41:19 -0300

    [5.18] Blog list e2e tests - step 18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,34 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'usertest',
+      password: 'test'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3001')
   })
 
   it('Login form is shown', function() {
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first()').type('usertest')
+      cy.get('input:last()').type('test')
+      cy.get('button').click()
+
+      cy.contains('logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first()').type('wrong')
+      cy.get('input:last()').type('wrong')
+      cy.get('button').click()
+
+      cy.contains('Wrong username or password')
+    })
   })
 })
